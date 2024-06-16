47bc7ebd60a30160b8a6cf4c5968e969f8ab1a39
chenpeng <chen@qq.com>
2024-06-16 23:26:09 +0800

    push commit02

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello,git!3");
         System.out.println("hello,git!4");
         System.out.println("push test");
+        System.out.println("push test02");
     }
 }
